4b6375be15511530fe6fa55809c6261e460f60b9
Samuel Lebeau <samuel.lebeau@gmail.com>
2012-01-07 00:19:08 +0100

    Add initializers and Rake tasks

---
+++ config/initializers/payme.rb
@@ -0,0 +1,17 @@
+Payme::Config.set_config(
+  :library => 'wordline',
+  :bin_path => 'bin/atos',
+  :pathfile => 'config/atos/pathfile'
+)
+
+class Payme::Response
+  # Fix Payme response parsing, should ignore first "field"
+  def parse_result(result)
+    parsed = Hash.new
+    result.shift # here we get rid of it
+    fields.each_with_index do |field, i|
+      parsed[field.to_sym] = result[i]
+    end
+    parsed
+  end
+end
